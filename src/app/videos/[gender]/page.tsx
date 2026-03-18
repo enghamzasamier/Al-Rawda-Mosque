523541a2b750103432e5761688cb683227cf419e
15112011 <xmmozana@gmail.com>
2026-03-18 10:26:00 +0200

    fix: really remove dynamic [gender] route to fix static export build conflict

--- src/app/videos/[gender]/page.tsx
+++
@@ -1,9 +0,0 @@
-// This dynamic route is unused - static routes /videos/male and /videos/female
-// take precedence. Required by Next.js static export: generateStaticParams must exist.
-export function generateStaticParams() {
-  return []
-}
-
-export default function GenderPage() {
-  return null
-}
